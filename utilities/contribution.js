58e78706c89c1d49f2a08c0dd80e3bbedbb016a4
Gage <gagelangdon@gmail.com>
2017-06-24 21:58:46 -0700

    stuff

--- utilities/contribution.js
+++ utilities/contribution.js
@@ -3,35 +3,27 @@ const Invoice = require('../models/invoice');
 const middleware = require('../utilities/middleware');
 
 const isFullfilled = async (invoiceID) => {
-    try {
-        let invoice = await Invoice.findById(invoiceID);
-        if (!invoice) throw ("invalid invoice id");
-        let contributions = await Contribution.find({ invoice: invoiceID });
-        if (!contributions || contributions.length < 1) return false;
-        let totalContributed = 0;
-        contributions.forEach(x => totalContributed += x.amount);
-        if (totalContributed >= invoice.total) return true;
-        else return false;
-    } catch (e) {
-        throw (e);
-    }
+    let invoice = await Invoice.findById(invoiceID);
+    if (!invoice) throw ("invalid invoice id");
+    let contributions = await Contribution.find({ invoice: invoiceID });
+    if (!contributions || contributions.length < 1) return false;
+    let totalContributed = 0;
+    contributions.forEach(x => totalContributed += x.amount);
+    if (totalContributed >= invoice.total) return true;
+    else return false;
 };
 const add = async (invoiceID, amount, userID) => {
-    try {
-        let foundInvoice = await Invoice.findById(invoiceID);
-        if (!foundInvoice) throw ("Invalid invoice id");
-        if (await isFullfilled(invoiceID)) throw ("Invoice already fulfulled");
-        let data = {
-            amount: amount,
-            invoice: foundInvoice._id,
-            user: userID,
-            dateCreated: new Date()
-        }
-        let newContribution = await Contribution.create(data);
-        newContribution = newContribution.populate('user')
-        return newContribution;
-    } catch (e) {
-        throw (e);
+    let foundInvoice = await Invoice.findById(invoiceID);
+    if (!foundInvoice) throw ("Invalid invoice id");
+    if (await isFullfilled(invoiceID)) throw ("Invoice already fulfulled");
+    let data = {
+        amount: amount,
+        invoice: foundInvoice._id,
+        user: userID,
+        dateCreated: new Date()
     }
+    let newContribution = await Contribution.create(data);
+    newContribution = newContribution.populate('user')
+    return newContribution;
 }
 module.exports = { isFullfilled, add }
